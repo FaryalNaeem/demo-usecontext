7a605eb3f78ba1997e6aeb531396cff41dc2f91b
Faryal Naeem <faryalnaeem66@yahoo.com>
2020-06-16 21:46:52 +0500

    update button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Parent from './Parent.js';
 
 function App() {
+    let [number, setNumber] = useState(45);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello World
+      <Parent num={number} ></Parent>
+      <button onClick={()=> { setNumber(++number)} } > Add number</button>
     </div>
   );
 }
 
-export default App;
+export default App; 
