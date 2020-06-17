1a39f1a24d8a3bc53cc183b55c729b9122ad5b97
Faryal Naeem <faryalnaeem66@yahoo.com>
2020-06-17 18:16:38 +0500

    created context

--- src/Child.js
+++ src/Child.js
@@ -2,9 +2,12 @@ import React, { useContext } from 'react';
 import ValueContext from './ValueContext';
 function Child() {
   let value = useContext(ValueContext);
+  console.log("value",value);
+  let updatebutton = value[1];
   return (
     <div>
-    Child number{value}
+    Child number{value[0]}
+    <button onClick={() => {updatebutton(++value[0])}}> Update number</button>
     </div>
   );
 }
